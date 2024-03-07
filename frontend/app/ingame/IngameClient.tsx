24a4aa2cb83c82b60311986ebadcd5c87fe57838
용상윤 <tkddbs1644@gmail.com>
2024-03-07 15:22:28 +0900

    feat(fe): Canvas Loading 기능 구현 S10P22A710-53

--- frontend/app/ingame/IngameClient.tsx
+++ frontend/app/ingame/IngameClient.tsx
@@ -1,9 +1,40 @@
 "use client";
 
+import React, { useState } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import Loading from "./components/Loading";
+import Box from "./models/Box";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 interface IngameClientProps {
   gameId: string;
 }
 
 export default function IngameClient({ gameId }: IngameClientProps) {
-  return <h1>R3F</h1>;
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <>
+      {loading && <Loading />}
+      <Canvas
+        camera={{
+          position: [0, 10, 5],
+        }}
+        onCreated={() => setLoading(false)}
+      >
+        <IngameThree />
+      </Canvas>
+    </>
+  );
+}
+
+function IngameThree() {
+  const { scene } = useThree();
+
+  return (
+    <>
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </>
+  );
 }
--- frontend/app/ingame/page.tsx
+++ frontend/app/ingame/page.tsx
@@ -1,10 +1,11 @@
+import "./index.css";
+
 import IngameClient from "./IngameClient";
 
-// TODO: 나중에 page를 ...slug 처리한 다음에 <IngameClient /> props로 넘겨야한다...
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div id="root">
       <IngameClient gameId="123" />
-    </main>
+    </div>
   );
 }
