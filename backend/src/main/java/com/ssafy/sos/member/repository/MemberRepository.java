06e76a273e7de43232d0a8334f7fda9fe7197c93
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-03-12 13:00:11 +0900

    feat: add jpa

--- backend/src/main/java/com/ssafy/sos/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/sos/member/repository/MemberRepository.java
@@ -4,6 +4,6 @@ import com.ssafy.sos.member.domain.Member;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long>{
+public interface MemberRepository{
 
 }
