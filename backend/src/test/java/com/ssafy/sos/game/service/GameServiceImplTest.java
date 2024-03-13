b47d844305cd84de6497d73068055bb951a41b45
김재형 <kjhstar971124@gmail.com>
2024-03-13 18:43:30 +0900

    feat: 해군 이동가능 위치 알고리즘 테스트케이스 구현 S10P22A710-130

--- backend/src/test/java/com/ssafy/sos/game/service/GameServiceImplTest.java
+++ backend/src/test/java/com/ssafy/sos/game/service/GameServiceImplTest.java
@@ -127,18 +127,36 @@ class GameServiceImplTest {
     @Test
     // 해군 이동 가능 위치 조회 (다른 해군이 서있을 시 해당 정점 도착 불가한지 검사)
     public void findMarineAvailableNode() {
-        // 검사1 : 다른 해군이 포함되지 않은 경우
+        // 검사1 : 다른 해군이 포함되지 않은 모든 이동가능한 경로 검증
         game = board.getGameMap().get(gameId);
-        game.getCurrentPosition()[0] = 1;
-        game.getCurrentPosition()[2] = 0;
-        game.getCurrentPosition()[3] = 0;
-        
+
         for (int i = 201; i <= 373; i++) {
-            HashMap<Integer, List<Integer>> marineMovableNode =  gameService.findMarineAvailableNode(gameId, 201);
+            HashMap<Integer, List<Integer>> marineMovableNode =  gameService.findMarineAvailableNode(gameId, i);
             // 기존 검사 완료 하였음 (로그 출력 정리 위해 주석처리)
-            System.out.println(marineMovableNode);
+            // System.out.println(marineMovableNode);
         }
 
-        // 검사2: 다른 해군이 포함된 경우
+        // 검사2 : 다른 해군이 포함된 경우 해당 정점은 선택할 수 없음
+        game.getCurrentPosition()[1] = 259;
+        game.getCurrentPosition()[2] = 284;
+
+        // 259번, 284번이 포함되어서는 안됨
+        Assertions.assertThat(gameService.findMarineAvailableNode(gameId, 270))
+                .doesNotContainKeys(259, 284);
+
+        // 검사3 : 이동 가능한 경로 여러개일 때 최단 경로 이동하는지 검사
+        List<Integer> check = new ArrayList<>();
+        check.add(201);
+        check.add(215);
+        // 201 - 215로 이동해야함 (반례: 201 - 8 - 214 - 215)
+        Assertions.assertThat(gameService.findMarineAvailableNode(gameId, 201).get(215))
+                .isEqualTo(check);
+
+        check.clear();
+        check.add(262);
+        check.add(273);
+        // 262-273로 이동해야함 (반례: 262 - 43 - 263 - 60 - 273)
+        Assertions.assertThat(gameService.findMarineAvailableNode(gameId, 262).get(273))
+                .isEqualTo(check);
     }
 }
