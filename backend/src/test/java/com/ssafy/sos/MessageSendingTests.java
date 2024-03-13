531f98177e54d3cf48b5c6d86f8752ccdc1ac504
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-03-13 15:58:00 +0900

    chore: remove not needed package S10P22A710-138

--- backend/src/test/java/com/ssafy/sos/MessageSendingTests.java
+++ backend/src/test/java/com/ssafy/sos/MessageSendingTests.java
@@ -4,7 +4,6 @@ import com.ssafy.sos.game.controller.MessageController;
 import com.ssafy.sos.game.domain.Board;
 import com.ssafy.sos.game.domain.Game;
 import com.ssafy.sos.game.message.client.ClientInitMessage;
-import com.ssafy.sos.game.message.server.ServerMessage;
 import com.ssafy.sos.game.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,19 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 @ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
