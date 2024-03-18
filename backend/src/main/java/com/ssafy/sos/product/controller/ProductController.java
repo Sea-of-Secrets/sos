d1e3c243a10881c7bfbc6640e26ac97e9f144c01
김재형 <kjhstar971124@gmail.com>
2024-03-18 18:19:41 +0900

    feat: 상품 등록 api 구현 S10P22A710-112

--- backend/src/main/java/com/ssafy/sos/product/controller/ProductController.java
+++ backend/src/main/java/com/ssafy/sos/product/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.ssafy.sos.product.controller;
 
+import com.ssafy.sos.product.domain.Product;
 import com.ssafy.sos.product.domain.ProductDTO;
 import com.ssafy.sos.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,10 @@ public class ProductController {
     @GetMapping("/shops")
     public ResponseEntity<List<ProductDTO>> getAllProductList() {
         return ResponseEntity.ok(productService.getProductList());
+    }
+
+    @PostMapping("/shops")
+    public ResponseEntity<Product> registerProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.registerProduct(product));
     }
 }
