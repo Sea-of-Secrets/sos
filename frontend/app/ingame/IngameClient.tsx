2cd5de9cc260443c1544fbfb0d04a99d51b2e04e
용상윤 <tkddbs1644@gmail.com>
2024-03-21 18:08:48 +0900

    chore(fe): 이재종 테스트 컴포넌트를 EjjTestController 라는 이름으로 test-components 폴더 내로 이동 S10P22A710-182

--- frontend/app/ingame/IngameClient.tsx
+++ frontend/app/ingame/IngameClient.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import style from "./EventHandler.module.scss";
-
 import React, { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 
@@ -12,25 +10,26 @@ import Turn from "./components/Turn";
 import SystemPrompt from "./components/SystemPrompt";
 
 import { gameSocket } from "~/sockets";
-import YongSangYoonTestController from "./test-components/YongSangYoonTestController";
 import { useGameLoading } from "./stores/useGameLoading";
+import EjjTestController from "./test-components/EjjTestController";
+import YsyTestController from "./test-components/YsyTestController";
 
 const { connect, send, subscribe, disconnect } = gameSocket;
 
 export default function IngameClient({ gameId }: { gameId: string }) {
   const { loading, setLoading } = useGameLoading();
 
-  const [nowNode, setNowNode] = useState();
-  const [nowNodePosition, setNowNodePosition] = useState([]);
-  const [nextMoveableNodes, setNextMoveableNodes] = useState([]);
-  const [nextNodeEdge, setNextNodeEdge] = useState([]);
+  // const [nowNode, setNowNode] = useState();
+  // const [nowNodePosition, setNowNodePosition] = useState([]);
+  // const [nextMoveableNodes, setNextMoveableNodes] = useState([]);
+  // const [nextNodeEdge, setNextNodeEdge] = useState([]);
 
-  // 소켓 통신을 통해 받게 될 데이터
-  const newMoveableNodes = [89, 106, 108, 126, 127, 128];
-  const newNodeEdge = [
-    [107, 309],
-    [309, 106],
-  ];
+  // // 소켓 통신을 통해 받게 될 데이터
+  // const newMoveableNodes = [89, 106, 108, 126, 127, 128];
+  // const newNodeEdge = [
+  //   [107, 309],
+  //   [309, 106],
+  // ];
 
   useEffect(() => {
     connect;
@@ -54,84 +53,17 @@ export default function IngameClient({ gameId }: { gameId: string }) {
         onCreated={() => setLoading(false)}
       >
         <IngameThree
-          nextMoveableNodes={nextMoveableNodes}
-          nextNodeEdge={nextNodeEdge}
+        // nextMoveableNodes={nextMoveableNodes}
+        // nextNodeEdge={nextNodeEdge}
         />
       </Canvas>
-      {/* <EventHandler
+      {/* <EjjTestController
         newMoveableNodes={newMoveableNodes}
         setNextMoveableNodes={setNextMoveableNodes}
         newNodeEdge={newNodeEdge}
         setNextNodeEdge={setNextNodeEdge}
       /> */}
-      <YongSangYoonTestController />
+      <YsyTestController />
     </>
   );
 }
-
-// function EventHandler({
-//   newMoveableNodes,
-//   setNextMoveableNodes,
-//   newNodeEdge,
-//   setNextNodeEdge,
-// }: any) {
-//   const { movePirate } = usePiece();
-//   const { pieceCamera, mapCamera } = useCamera();
-//   const [isNextMoveableNodes, setIsNextMoveableNodes] = useState(true);
-//   const [isNewNodeEdge, setIsNewNodeEdge] = useState(true);
-//   const [isFocused, setIsFocused] = useState(false);
-//   const [isMoved, setIsMoved] = useState(false);
-
-//   // 이동 가능 노드 표시
-//   const handleNextMoveableNodes = () => {
-//     setIsNextMoveableNodes(!isNextMoveableNodes);
-//     setNextMoveableNodes(isNextMoveableNodes ? newMoveableNodes : []);
-//   };
-
-//   // 다음 노드 경로 표시
-//   const handleNextNodeEdge = () => {
-//     setIsNewNodeEdge(!isNewNodeEdge);
-//     setNextNodeEdge(isNewNodeEdge ? newNodeEdge : []);
-//   };
-
-//   // 말 포커싱
-//   const handleFocusPiece = () => {
-//     if (!isFocused) {
-//       pieceCamera([
-//         INGAME_GRAPH["107"].position.x,
-//         INGAME_GRAPH["107"].position.y,
-//       ]);
-//       setIsFocused(true);
-//     } else {
-//       mapCamera();
-//       setIsFocused(false);
-//     }
-//   };
-
-//   // 말 이동
-//   const handleMovePiece = () => {
-//     setIsMoved(!isMoved);
-//     movePirate(
-//       isMoved
-//         ? [INGAME_GRAPH["107"].position.x, INGAME_GRAPH["107"].position.y]
-//         : [-30, 100],
-//     );
-//   };
-
-//   return (
-//     <div className={style.box}>
-//       <button className={style.greenbutton} onClick={handleNextMoveableNodes}>
-//         {isNextMoveableNodes ? "이동 가능 노드 표시" : "이동 가능 노드 미표시"}
-//       </button>
-//       <button className={style.greenbutton} onClick={handleNextNodeEdge}>
-//         {isNewNodeEdge ? "다음 경로 표시" : "다음 경로 미표시"}
-//       </button>
-//       <button className={style.greenbutton} onClick={handleFocusPiece}>
-//         {isFocused ? "전체 포커싱" : "말 포커싱"}
-//       </button>
-//       <button className={style.greenbutton} onClick={handleMovePiece}>
-//         말 이동
-//       </button>
-//     </div>
-//   );
-// }
