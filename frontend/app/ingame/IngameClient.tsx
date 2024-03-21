b85d7bb936f8f840decc1aac34fa315cbefa72ac
용상윤 <tkddbs1644@gmail.com>
2024-03-21 17:35:17 +0900

    refactor(fe): useGameLoading() store hook 구현 S10P22A710-182

--- frontend/app/ingame/IngameClient.tsx
+++ frontend/app/ingame/IngameClient.tsx
@@ -13,13 +13,13 @@ import SystemPrompt from "./components/SystemPrompt";
 
 import { gameSocket } from "~/sockets";
 import YongSangYoonTestController from "./test-components/YongSangYoonTestController";
+import { useGameLoading } from "./stores/useGameLoading";
 
 const { connect, send, subscribe, disconnect } = gameSocket;
 
-// TODO: Canvas만 로딩됐다고 끝이 아니라 안에 모델, 텍스쳐도 다 로딩이 되어야함.
-// 나중에 이 로딩을 상태관리로 만들자.
 export default function IngameClient({ gameId }: { gameId: string }) {
-  const [loading, setLoading] = useState(true);
+  const { loading, setLoading } = useGameLoading();
+
   const [nowNode, setNowNode] = useState();
   const [nowNodePosition, setNowNodePosition] = useState([]);
   const [nextMoveableNodes, setNextMoveableNodes] = useState([]);
