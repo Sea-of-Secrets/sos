3d52eb510b2a001eb91f7b784d1119d641c5058f
용상윤 <tkddbs1644@gmail.com>
2024-03-29 10:06:25 +0900

    test(fe): CORS 테스트 1

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'standalone',
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://j10a710.p.ssafy.io",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
