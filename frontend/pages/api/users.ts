31e364d2214012648610fe8544579bd093a3269f
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-03-29 13:16:01 +0900

    fix: mypage nft listing

--- frontend/pages/api/users.ts
+++ frontend/pages/api/users.ts
@@ -10,21 +10,20 @@ export default async function handler(
   res: NextApiResponse<ResponseData>,
 ) {
   try {
+    const cookies = req.headers.cookie?.split('; ');
+    let accessCookie;
+    let refreshCookie;
+
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split('='); // 쿠키 문자열을 '=' 기준으로 분리하여 키와 값을 추출
+        if (key === 'access') {
+            accessCookie = value;
+        } else if (key === 'refresh') {
+            refreshCookie = value;
+        }
+    }
+
     if (req.method === "GET") {
-    
-      const cookies = req.headers.cookie?.split('; ');
-      let accessCookie;
-      let refreshCookie;
-
-      for (const cookie of cookies) {
-          const [key, value] = cookie.split('='); // 쿠키 문자열을 '=' 기준으로 분리하여 키와 값을 추출
-          if (key === 'access') {
-              accessCookie = value;
-          } else if (key === 'refresh') {
-              refreshCookie = value;
-          }
-      }
-
       const response = await http.get("/users", {
         headers: {
           Cookie: `access=${accessCookie}; refresh=${refreshCookie}`
@@ -32,6 +31,27 @@ export default async function handler(
       });
       
       return res.status(response.status).json(response.data);
+    } else {
+      if (req.body.type === "get") {
+        const response = await http.get("/nft/wallet", {
+          headers: {
+            Cookie: `access=${accessCookie}; refresh=${refreshCookie}`
+          }
+        });
+        
+        return res.status(response.status).json(response.data);
+      } else if (req.body.type === "post") {
+        const response = await http.post("/nft/wallet", {
+          headers: {
+            Cookie: `access=${accessCookie}; refresh=${refreshCookie}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(null) // JSON.stringify를 사용하여 본문을 문자열로 변환
+        });
+        
+        return res.status(response.status).json(response.data);
+      }
+      
     }
 
     res.status(400).json({ message: "400" });
