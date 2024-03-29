d987ee067b5a5b90d72659816fbc42882eaf2091
용상윤 <tkddbs1644@gmail.com>
2024-03-29 17:16:17 +0900

    fix(fe): source 제거하고 도메인 지정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,29 +5,24 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        // matching all API routes
+        //source: "/api/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "*", // 모든 Origin에 대해 허용
+            value: "http://j10a710.p.ssafy.io",
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
-        // headers: [
-        //   { key: "Access-Control-Allow-Credentials", value: "true" },
-        //   {
-        //     key: "Access-Control-Allow-Origin",
-        //     value: "http://j10a710.p.ssafy.io",
-        //   },
-        //   {
-        //     key: "Access-Control-Allow-Methods",
-        //     value: "GET, POST, OPTIONS",
-        //   },
-        //   {
-        //     key: "Access-Control-Allow-Headers",
-        //     value:
-        //       "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
-        //   },
-        // ],
       },
     ];
   },
