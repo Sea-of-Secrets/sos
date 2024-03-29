878d182f7946ffd8227366e5d697f751324b591f
용상윤 <tkddbs1644@gmail.com>
2024-03-29 16:50:33 +0900

    fix(fe): headers() 추가, false, true, 80포트 도메인 지정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,29 +10,29 @@ const nextConfig = {
       },
     ];
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "false" },
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "*",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET, POST, OPTIONS",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://j10a710.p.ssafy.io",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
