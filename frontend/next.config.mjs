f49cfe27bb346aa84f0e466e989b07283282dbb5
용상윤 <tkddbs1644@gmail.com>
2024-03-29 10:30:48 +0900

    test(fe): next.config에 credentials true, source 추가

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,21 +5,22 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: "/api/:path*",
         headers: [
-          // { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Credentials", value: "false" },
           {
             key: "Access-Control-Allow-Origin",
             value: "*",
           },
-          // {
-          //   key: "Access-Control-Allow-Methods",
-          //   value: "GET,POST",
-          // },
-          // {
-          //   key: "Access-Control-Allow-Headers",
-          //   value:
-          //     "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
-          // },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
         ],
       },
     ];
