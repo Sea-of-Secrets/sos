ba36cdcad3983347e6987e5adc8696e5dffebf94
용상윤 <tkddbs1644@gmail.com>
2024-03-29 11:20:32 +0900

    config(fe): fe env 설정

--- frontend/_lib/http.ts
+++ frontend/_lib/http.ts
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-const SERVER_API_END_POINT = "http://j10a710.p.ssafy.io:8081/api";
-const CLIENT_API_END_POINT = "http://j10a710.p.ssafy.io:3000/api";
-// const SERVER_API_END_POINT = "http://localhost:8080/";
-// const CLIENT_API_END_POINT = "http://localhost:3000/api";
+const { CLIENT_API_END_POINT, SERVER_API_END_POINT } = process.env;
+
+const _SERVER_API_END_POINT =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : SERVER_API_END_POINT;
+const _CLIENT_API_END_POINT =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : CLIENT_API_END_POINT;
 
 export const http = axios.create({
-  baseURL: SERVER_API_END_POINT,
+  baseURL: _SERVER_API_END_POINT,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
@@ -14,7 +20,7 @@ export const http = axios.create({
 });
 
 export const client = axios.create({
-  baseURL: CLIENT_API_END_POINT,
+  baseURL: _CLIENT_API_END_POINT,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
