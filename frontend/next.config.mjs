33b038e293462bc8fcf46a6d090d16fbdad49b8f
용상윤 <tkddbs1644@gmail.com>
2024-03-29 16:45:54 +0900

    fix(fe): rewrites의 destination에 3000포트를 지정, headers() 제거

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,33 +6,33 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination: "http://j10a710.p.ssafy.io/:path*",
+        destination: "http://j10a710.p.ssafy.io:3000/:path*",
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "false" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "false" },
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "*",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET, POST, OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
