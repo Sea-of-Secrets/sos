9e1e30492cf21f2ead08d15eaf0874b5e0b84247
Juyeori <dlwndus0728@ajou.ac.kr>
2024-03-31 16:30:49 +0900

    feat: oauth button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import styles from "./page.module.scss";
 import Client from "./Client";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -15,22 +16,31 @@ export default function Home() {
             마이페이지
           </Link>
           <a
-            className="button"
             href="http://localhost:8080/oauth2/authorization/naver"
           >
-            naver login
+            <Image 
+              src="/images/btnG_완성형.png"
+              alt="Naver login"
+              width={200}
+              height={50}/>
           </a>
           <a
-            className="button"
             href="https://j10a710.p.ssafy.io/api/oauth2/authorization/kakao"
           >
-            kakao login
+            <Image 
+              src="/images/kakao_login_medium_narrow.png"
+              alt="Kakao login"
+              width={200}
+              height={50}/>
           </a>
           <a
-            className="button"
             href="https://j10a710.p.ssafy.io/api/oauth2/authorization/google"
           >
-            google login
+            <Image 
+              src="/images/web_light_sq_ctn@4x.png"
+              alt="Google login"
+              width={200}
+              height={50}/>
           </a>
           <Link className="button" href="/test">
             차린건 없지만 테스트라도...
