e9a48069a643f70787b6a2004aeb5562e9b5af78
용상윤 <tkddbs1644@gmail.com>
2024-04-01 12:49:16 +0900

    fix(fe): socket api 매핑 함수 추가하여 리팩터링

--- frontend/_lib/http.ts
+++ frontend/_lib/http.ts
@@ -14,6 +14,13 @@ export const getBaseClientUrl = () => {
   return process.env.NEXT_PUBLIC_CLIENT_API_END_POINT;
 };
 
+export const getBaseSocketServerUrl = () => {
+  if (process.env.NODE_ENV === "development") {
+    return "ws://localhost:8080/sos";
+  }
+  return `${process.env.NEXT_PUBLIC_SERVER_SOCKET_API_END_POINT}/sos`;
+};
+
 export const request = axios.create({
   baseURL: "",
   timeout: 5000,
