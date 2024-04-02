ddfee17985f706a5d4861a4fd2c0cccc951d73d4
용상윤 <tkddbs1644@gmail.com>
2024-04-02 17:56:09 +0900

    feat(fe): 비회원은 로그인으로 유도하는 기능 구현 S10P22A710-999

--- frontend/app/render/shop/index.tsx
+++ frontend/app/render/shop/index.tsx
@@ -8,15 +8,25 @@ import Container from "../components/Container";
 import { useCamera } from "../stores/useCamera";
 import { useGatcha } from "../stores/useGatch";
 import Gatcha from "./Gatcha";
+import { useScreenControl } from "../stores/useScreenControl";
+
+// TODO: 로그인 상태 관리
+const isUserLogin = true;
 
 export default function Shop() {
   const { gatchaState, setGatchaState } = useGatcha();
-  const { ShopGatchaScreen } = useCamera();
+  const { ShopGatchaScreen, LoginScreen } = useCamera();
+  const { screen, setScreen, setMainScreen } = useScreenControl();
 
   const handleClickGatchaView = useCallback(() => {
     setGatchaState("GATCHA_READY");
     ShopGatchaScreen();
   }, [setGatchaState, ShopGatchaScreen]);
+
+  const handleClickLogin = useCallback(() => {
+    setGatchaState("GATCHA_PREV");
+    setScreen("LOGIN");
+  }, [setGatchaState, setScreen]);
 
   if (gatchaState !== "GATCHA_PREV") {
     return <Gatcha />;
@@ -24,7 +34,11 @@ export default function Shop() {
 
   return (
     <Container position="right">
-      <Button onClick={handleClickGatchaView}>700G</Button>
+      {isUserLogin ? (
+        <Button onClick={handleClickGatchaView}>700G</Button>
+      ) : (
+        <Button onClick={handleClickLogin}>로그인</Button>
+      )}
     </Container>
   );
 }
