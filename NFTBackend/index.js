ced298d0dfa23c8da76c720d2d047aec79ef7170
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-04-03 16:34:24 +0900

    feat: 용량 제한 해제

--- NFTBackend/index.js
+++ NFTBackend/index.js
@@ -6,4 +6,12 @@ const port = 4000;
 
 app.use(express.json());
 app.use(nftRouter);
+app.use(express.json({
+    limit : "50mb"
+}));
+app.use(express.urlencoded({
+    limit:"50mb",
+    extended: false
+}));
+
 app.listen(port);
