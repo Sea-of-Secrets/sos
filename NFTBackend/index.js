8db21c3f50b1f65a712ae6af57706ceeca5e1fb6
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-04-03 16:58:19 +0900

    hotfix: 용량 제한 해제

--- NFTBackend/index.js
+++ NFTBackend/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const nftRouter = require('./src/controller/NFTController');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 4000;
 
 app.use(nftRouter);
-app.use(express.json({
-    limit : "50mb"
-}));
-app.use(express.urlencoded({
-    limit:"50mb",
-    extended: false
+
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit:50000
 }));
 
 app.listen(port);
