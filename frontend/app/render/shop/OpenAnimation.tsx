d1131d036007071d1eaca350bab5a8294aef9043
이재종 <wowhd45@naver.com>
2024-04-04 03:41:29 +0900

    fix: 가챠 애니메이션 수정 S10P22A710-226

--- frontend/app/render/shop/OpenAnimation.tsx
+++ frontend/app/render/shop/OpenAnimation.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { useGatcha } from "../stores/useGatch";
+import { LoopOnce } from "three";
 
 export default function Open() {
   const { gatchaState, setGatchaState } = useGatcha();
   const { scene, animations } = useGLTF("/gatcha/scene.gltf");
   const { actions } = useAnimations(animations, scene);
-  const [animationPlayed, setAnimationPlayed] = useState(false);
 
   useEffect(() => {
     if (gatchaState === "GATCHA_READY") {
       const action = actions["Take 001"];
       if (action) {
         action.clampWhenFinished = true;
-        action.startAt(2).setDuration(3).play().halt(2);
+        action.reset().setDuration(2).play().setLoop(LoopOnce, 0);
       }
     }
   }, [gatchaState]);
