41d8ad5d9670c2a3a854f3cb1d3a65ed3be4ff59
용상윤 <tkddbs1644@gmail.com>
2024-04-03 17:21:42 +0900

    feat(fe): Gatcha 모달 기능 구현 S10P22A710-999

--- frontend/app/api/shops.ts
+++ frontend/app/api/shops.ts
@@ -1,4 +1,5 @@
-import { request, getBaseClientUrl } from "../../_lib/http";
+import { getAccessToken, getRefreshToken } from "~/store/auth";
+import { request, getBaseClientUrl, getBaseServerUrl } from "../../_lib/http";
 
 type Product = {};
 
@@ -6,3 +7,21 @@ export const getProductList = async () => {
   const res = await request.get<Product[]>(`${getBaseClientUrl()}/shops`);
   return res;
 };
+
+export const postGatcha = async () => {
+  const access = getAccessToken();
+  const refresh = getRefreshToken();
+
+  const res = await request.post(`${getBaseServerUrl()}/products/shops`, {
+    headers: {
+      Cookie: `access=${access}; refresh=${refresh};`,
+      Authorization: `${access},${refresh}`,
+    },
+  });
+  return res;
+};
+
+// export const postGatcha = async () => {
+//   const res = await request.post(`${getBaseServerUrl()}/products/random`);
+//   return res;
+// };
