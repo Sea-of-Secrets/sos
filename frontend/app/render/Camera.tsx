0da9391891e8e1191076646d3e23770383b2f9fd
이재종 <wowhd45@naver.com>
2024-04-04 00:18:30 +0900

    fix: 세션스토리지로 인트로 애니메이션 시작 S10P22A710-226

--- frontend/app/render/Camera.tsx
+++ frontend/app/render/Camera.tsx
@@ -7,38 +7,29 @@ import { useScreenControl } from "./stores/useScreenControl";
 export default function Camera() {
   const cameraRef = useRef<CameraControls>(null!);
   const { setCamera, mainScreen, introScreen } = useCamera();
-  const {
-    startIntro,
-    setStartIntroScreen,
-    setScreen,
-    setMainScreen,
-    setShowLogo,
-  } = useScreenControl();
+  const { showIntro, setScreen, setMainScreen, setShowLogo } =
+    useScreenControl();
 
   useEffect(() => {
     setCamera(cameraRef);
-    // if (startIntro) {
-    mainScreen();
-    setMainScreen();
-    //   mainScreen();
-    //   setMainScreen();
-    // } else {
-    //   introScreen();
-    //   setScreen("START");
-    //   setStartIntroScreen();
-    //   setTimeout(() => {
-    //     setShowLogo();
-    //   }, 4000);
-    // }
+    if (showIntro) {
+      mainScreen();
+      setMainScreen();
+    } else {
+      introScreen();
+      setScreen("START");
+      setTimeout(() => {
+        setShowLogo();
+      }, 4000);
+    }
   }, [
-    startIntro,
+    showIntro,
     setMainScreen,
     setShowLogo,
     setScreen,
     setCamera,
     mainScreen,
     introScreen,
-    setStartIntroScreen,
   ]);
 
   return (
