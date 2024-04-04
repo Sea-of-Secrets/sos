c41399b51f03da853b5a3db9eac3f5b1ac7e2b5b
Mrrchoi <csj9912@naver.com>
2024-04-04 23:17:56 +0900

    feat: 출석 중복 에러 해결

--- backend/src/main/java/com/ssafy/sos/user/repository/TodayVisitedRepository.java
+++ backend/src/main/java/com/ssafy/sos/user/repository/TodayVisitedRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.sos.user.repository;
 import com.ssafy.sos.user.domain.TodayVisited;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodayVisitedRepository extends CrudRepository<TodayVisited, String> {
+public interface TodayVisitedRepository extends CrudRepository<TodayVisited, Long> {
 }
