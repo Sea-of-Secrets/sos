6d92fa9938bd0b7c2c4751e6ea85e9c2f1e3b822
yong <tkddbs1644@gmail.com>
2024-04-06 22:44:23 +0900

    chore(fe): 빠른매칭 알림문구수정

--- frontend/app/render/fastmatching/index.tsx
+++ frontend/app/render/fastmatching/index.tsx
@@ -49,7 +49,7 @@ export default function FastMatching() {
   const handleClickCheckButton = async () => {
     const TEMP_KEY = window.localStorage.getItem("TEMP_KEY");
     if (TEMP_KEY !== "유일무이이주희") {
-      window.alert("점검 중인 기능이에요");
+      window.alert("발표 시연을 위해 잠시 막아두었어요");
       return;
     }
 
