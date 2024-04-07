58a3c85f06aa73c93633747c67b2d89d2a517ff0
yong <tkddbs1644@gmail.com>
2024-04-07 19:38:19 +0900

    refactor(fe): GatchaCard 개발을 위해 GatchaCard 컴포넌트 분리

--- frontend/app/render/shop/Gatcha.tsx
+++ frontend/app/render/shop/Gatcha.tsx
@@ -4,9 +4,10 @@ import styled from "@emotion/styled";
 import Button from "../components/BackButton";
 import Overlay from "../components/Overlay";
 import { useRandomGatcha } from "../stores/useRandomGatcha";
+import GatchaCard from "./GatchaCard";
 
 export default function Gatcha() {
-  const { loading, randomGatcha, fetchRandomGatcha } = useRandomGatcha();
+  const { loading, fetchRandomGatcha } = useRandomGatcha();
 
   const handleClickBackButton = useCallback(() => {
     window.location.href = "/";
@@ -16,70 +17,11 @@ export default function Gatcha() {
     fetchRandomGatcha();
   }, [fetchRandomGatcha]);
 
-  if (loading || !randomGatcha) {
-    return (
-      <Overlay sens="LOW">
-        <Container></Container>
-      </Overlay>
-    );
-  }
-
   return (
     <Overlay sens="LOW">
       <Container>
-        <Button onClick={handleClickBackButton} />
-        <Wrapper>
-          <CenterBox>
-            <NftCard
-              className={randomGatcha ? "animated" : ""}
-              style={{
-                width: "30rem",
-                height: "28rem",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                border: "none",
-                borderRadius: "10px",
-                padding: "10px",
-              }}
-            >
-              <h2
-                style={{
-                  fontSize: "3rem",
-                  color: `${getColorToGrade(randomGatcha.grade)}`,
-                }}
-              >
-                {randomGatcha.grade}
-              </h2>
-              <img
-                src={randomGatcha.imgUrl || ""}
-                alt=""
-                style={{
-                  maxWidth: "300px",
-                  maxHeight: "300px",
-                }}
-              />
-              <p
-                style={{
-                  fontSize: "2rem",
-                  color: "#b2adad",
-                }}
-                className="mt-2"
-              >
-                {randomGatcha.name || ""}
-              </p>
-              {randomGatcha.hasItemAlready && (
-                <h3
-                  style={{
-                    color: "#b2adad",
-                  }}
-                >
-                  이미 가지고 있는 상품이에요
-                </h3>
-              )}
-            </NftCard>
-          </CenterBox>
-        </Wrapper>
+        {!loading && <Button onClick={handleClickBackButton} />}
+        <GatchaCard />
       </Container>
     </Overlay>
   );
@@ -89,47 +31,3 @@ const Container = styled.div`
   min-width: 800px;
   height: 100%;
 `;
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CenterBox = styled.div`
-  width: 400px;
-  height: 500px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const NftCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  padding: 20px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-  transition: transform 1s ease-in-out; // 애니메이션 효과 적용
-`;
-
-const getColorToGrade = (grade: string) => {
-  grade = grade.toLowerCase();
-  if (grade === "legendary") {
-    return "#ffc800";
-  }
-
-  if (grade === "rare") {
-    return "#73e337";
-  }
-
-  if (grade === "common") {
-    return "#b2adad";
-  }
-
-  return "white";
-};
---
+++ frontend/app/render/shop/GatchaCard.tsx
@@ -0,0 +1,109 @@
+import styled from "@emotion/styled";
+import { useRandomGatcha } from "../stores/useRandomGatcha";
+
+export default function GatchaCard() {
+  const { randomGatcha } = useRandomGatcha();
+
+  if (!randomGatcha) {
+    return null;
+  }
+
+  return (
+    <Wrapper>
+      <CenterBox>
+        <NftCard
+          className={randomGatcha ? "animated" : ""}
+          style={{
+            width: "30rem",
+            height: "28rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            border: "none",
+            borderRadius: "10px",
+            padding: "10px",
+          }}
+        >
+          <h2
+            style={{
+              fontSize: "3rem",
+              color: `${getColorToGrade(randomGatcha.grade)}`,
+            }}
+          >
+            {randomGatcha.grade}
+          </h2>
+          <img
+            src={randomGatcha.imgUrl || ""}
+            alt=""
+            style={{
+              maxWidth: "300px",
+              maxHeight: "300px",
+            }}
+          />
+          <p
+            style={{
+              fontSize: "2rem",
+              color: "#b2adad",
+            }}
+            className="mt-2"
+          >
+            {randomGatcha.name || ""}
+          </p>
+          {randomGatcha.hasItemAlready && (
+            <h3
+              style={{
+                color: "#b2adad",
+              }}
+            >
+              이미 가지고 있는 상품이에요
+            </h3>
+          )}
+        </NftCard>
+      </CenterBox>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CenterBox = styled.div`
+  width: 400px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NftCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  transition: transform 1s ease-in-out; // 애니메이션 효과 적용
+`;
+
+const getColorToGrade = (grade: string) => {
+  grade = grade.toLowerCase();
+  if (grade === "legendary") {
+    return "#ffc800";
+  }
+
+  if (grade === "rare") {
+    return "#73e337";
+  }
+
+  if (grade === "common") {
+    return "#b2adad";
+  }
+
+  return "white";
+};
